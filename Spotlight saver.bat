f59e4218a837c66bc1c773f4d9b2ce17713e3226
Lorenzo Pieroni <45267875+Lorenzo-Pieroni@users.noreply.github.com>
2025-04-13 15:42:55 +0200

    Add files via upload

--- Spotlight saver.bat
+++ Spotlight saver.bat
@@ -1,24 +1,29 @@
 @if (@X)==(@Y) @end /* JScript comment
   @echo off
-  setx Spotlight "%~dp0"
+  set "PathTemp=%~dp0"
+  set "PathTemp=%PathTemp:"=%"
+  if %PathTemp:~-1%==\ set "PathTemp=%PathTemp:~0,-1%"
+  setx Spotlight "%PathTemp%"
+  set "spot=%Spotlight:"=%"
+
   if not "%1"=="am_admin" (
   powershell start -verb runas '%0' am_admin & exit /b
   ) else (
-      if not exist "%Spotlight%\Spotlight" mkdir "%Spotlight%\Spotlight"
-      cd /D "%Spotlight%\Spotlight"
+      if not exist "%spot%\Spotlight" mkdir "%spot%\Spotlight"
+      cd /D "%spot%\Spotlight"
       forfiles /M *.jpg /C "cmd /c rename @file @fname"
-      robocopy "%LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "%Spotlight%\Spotlight" /s /min:300000 /NFL /NDL /NJH /NJS /nc /ns /np
+      robocopy "%LocalAppData%\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "%spot%\Spotlight" /s /min:300000 /NFL /NDL /NJH /NJS /nc /ns /np
       ren * *.jpg
       echo PROCESS IN PROGRESS WAIT UNTIL THE WINDOW CLOSES...
-      for /R "%Spotlight%\Spotlight" %%G in (*.jpg) do call :checkDimension "%%G"
+      for /R "%spot%\Spotlight" %%G in (*.jpg) do call :checkDimension "%%G"
     )
 
 :checkDimension
 
-	rem :: the first argument is the script name as it will be used for proper help message
-	cscript //E:JScript //nologo "%~f0" %*
+rem :: the first argument is the script name as it will be used for proper help message
+cscript //E:JScript //nologo "%~f0" %*
 
-	exit /b %errorlevel%
+exit /b %errorlevel%
 
 @if (@X)==(@Y) @end JScript comment */
 var ARGS = WScript.Arguments;
@@ -27,8 +32,12 @@ var wshShell = new ActiveXObject("WScript.Shell");
 var filename=ARGS.Item(0);
 
 var img=new ActiveXObject("WIA.ImageFile");
-img.LoadFile(filename);
-if(img.Width<1400){
-    var cmd = "cmd.exe /c del " + filename
-    wshShell.Run(cmd);
+try {
+    img.LoadFile(filename);
+    if (img.Width < 1400) {
+        var cmd = "cmd.exe /c del \"" + filename + "\"";
+        wshShell.Run(cmd);
+    }
+} catch (e) {
+    WScript.Echo("Errore nel caricamento dell'immagine: " + e.message);
 }
